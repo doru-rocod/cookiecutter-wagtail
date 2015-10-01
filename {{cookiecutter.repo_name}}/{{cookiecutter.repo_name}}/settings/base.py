6e78e4604c48ec625eb68d41530e03001c76bdd3
Franklin Kingma <franklin@fourdigits.nl>
2015-10-01 10:09:39 +0200

    Move cache

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/settings/base.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/settings/base.py
@@ -140,21 +140,6 @@ COMPRESS_PRECOMPILERS = (
     ('text/x-scss', 'django_libsass.SassCompiler'),
 )
 
-
-# Use Redis as the cache backend for extra performance
-
-CACHES = {
-    'default': {
-        'BACKEND': 'redis_cache.cache.RedisCache',
-        'LOCATION': '127.0.0.1:6379',
-        'KEY_PREFIX': '{{ cookiecutter.repo_name }}',
-        'OPTIONS': {
-            'CLIENT_CLASS': 'redis_cache.client.DefaultClient',
-        }
-    }
-}
-
-
 # Use Elasticsearch as the search backend for extra performance and better search results
 
 WAGTAILSEARCH_BACKENDS = {
--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/settings/production.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/settings/production.py
@@ -8,6 +8,18 @@ from .base import *
 DEBUG = False
 TEMPLATE_DEBUG = False
 
+# Use Redis as the cache backend for extra performance
+
+CACHES = {
+    'default': {
+        'BACKEND': 'redis_cache.cache.RedisCache',
+        'LOCATION': '127.0.0.1:6379',
+        'KEY_PREFIX': '{{ cookiecutter.repo_name }}',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'redis_cache.client.DefaultClient',
+        }
+    }
+}
 
 # Compress static files offline and minify CSS
 # http://django-compressor.readthedocs.org/en/latest/settings/#django.conf.settings.COMPRESS_OFFLINE
