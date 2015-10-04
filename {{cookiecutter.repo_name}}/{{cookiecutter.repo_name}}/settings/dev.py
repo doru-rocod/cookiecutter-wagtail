83e050f115190d7cc60c227a92295c18da969704
Franklin Kingma <franklin@fourdigits.nl>
2015-10-04 14:38:10 +0200

    Add nosetest

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/settings/dev.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/settings/dev.py
@@ -17,6 +17,27 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 CELERY_EAGER_PROPAGATES_EXCEPTIONS = True
 CELERY_ALWAYS_EAGER = True
 
+INSTALLED_APPS = ['django_nose'] + INSTALLED_APPS
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+NOSE_ARGS = [
+    '--with-xunit',
+    '--with-xcoverage',
+    '--cover-package=ecommerce',
+    '--cover-erase',
+    '--nocapture',
+    '--with-doctest',
+    '--ignore-files=^[0-9]{4}.*',
+    '--ignore-files=^fabfile',
+    '--ignore-files=^setup',
+    '--ignore-files=^meta.py',
+    '--ignore-files=config.py',
+    '--ignore-files=acc.py',
+    '--ignore-files=dev.py',
+    '--ignore-files=prd.py',
+    '--ignore-files=tst.py',
+    '--verbosity=2',
+    '--exe',
+    ]
 
 try:
     from .local import *
---
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/settings/jenkins.py
@@ -0,0 +1,23 @@
+from .base import *
+
+INSTALLED_APPS = ['django_nose'] + INSTALLED_APPS
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+NOSE_ARGS = [
+    '--with-xunit',
+    '--with-xcoverage',
+    '--cover-package=ecommerce',
+    '--cover-erase',
+    '--nocapture',
+    '--with-doctest',
+    '--ignore-files=^[0-9]{4}.*',
+    '--ignore-files=^fabfile',
+    '--ignore-files=^setup',
+    '--ignore-files=^meta.py',
+    '--ignore-files=config.py',
+    '--ignore-files=acc.py',
+    '--ignore-files=dev.py',
+    '--ignore-files=prd.py',
+    '--ignore-files=tst.py',
+    '--verbosity=2',
+    '--exe',
+    ]
