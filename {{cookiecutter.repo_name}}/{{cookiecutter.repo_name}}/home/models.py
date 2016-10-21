629bcdc961e036eb33a1dfa56b802f639c2865e3
Kees Hink <keeshink@gmail.com>
2016-10-21 10:37:46 +0200

    pep8 Docstrings

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/home/models.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/home/models.py
@@ -1,3 +1,5 @@
+"""Homepage-related models and functions."""
+
 from __future__ import unicode_literals
 
 from django.db import models
@@ -6,4 +8,6 @@ from wagtail.wagtailcore.models import Page
 
 
 class HomePage(Page):
+	"""Model for the home page."""
+
     pass
