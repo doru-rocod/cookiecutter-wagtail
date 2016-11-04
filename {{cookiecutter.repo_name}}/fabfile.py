459df02ddb071fb045df72ee9e2549ed2523a947
Franklin Kingma <franklin@fourdigits.nl>
2016-11-04 14:31:22 +0100

    Updated fabric

--- {{cookiecutter.repo_name}}/fabfile.py
+++ {{cookiecutter.repo_name}}/fabfile.py
@@ -4,18 +4,23 @@ from fabric.api import *
 from fabric.context_managers import cd
 
 env.roledefs = {
-    'production': [],
+    'production': ['worker04.fourdigits.nl', 'worker05.fourdigits.nl'],
     'staging': [],
+    'test': ['lightspeed.fourdigits.nl']
 }
 
-env.user = 'root'
+env.user = '{{ cookiecutter.repo_name }}'
+
+env.always_use_pty = False
+env.forward_agent = True
 env.use_ssh_config = True
+env.first_worker = env.roledefs['production'][0]
 
 
-@roles('staging')
+@hosts(env.first_worker)
 def deploy_staging():
     """Deploy to staging environment."""
-    with cd('/django/{{ cookiecutter.repo_name }}'):
+    with cd('/home/{{ cookiecutter.repo_name }}/application/{{ cookiecutter.repo_name }}'):
         run('git pull origin master')
         run('./bin/pip install -r requirements/prd.txt')
         run('bin/python manage.py migrate --noinput --settings={{ cookiecutter.repo_name }}.settings.production')
@@ -23,18 +28,6 @@ def deploy_staging():
         run('bin/python manage.py compress --settings={{ cookiecutter.repo_name }}.settings.production')
         run('bin/python manage.py update_index --settings={{ cookiecutter.repo_name }}.settings.production')
 
-        run('touch /etc/uwsgi/vassels/{{ cookiecutter.repo_name }}.ini')
-
-
-@roles('production')
-def deploy_production():
-    """Deploy to production."""
-    with cd('/django/{{ cookiecutter.repo_name }}'):
-        run('git pull origin master')
-        run('bin/pip install -r requirements/prd.txt')
-        run('bin/python manage.py migrate --noinput --settings={{ cookiecutter.repo_name }}.settings.production')
-        run('bin/python manage.py collectstatic --noinput --settings={{ cookiecutter.repo_name }}.settings.production')
-        run('bin/python manage.py compress --settings={{ cookiecutter.repo_name }}.settings.production')
-        run('bin/python manage.py update_index --settings={{ cookiecutter.repo_name }}.settings.production')
-
-        run('touch /etc/uwsgi/vassels/{{ cookiecutter.repo_name }}.ini')
+def reload():
+    with cd('/home/{{ cookiecutter.repo_name }}/'):
+        run('touch uwsgi-reload')
